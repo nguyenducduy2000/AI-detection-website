0130f74a004ac6e0c9b13b9eb32c1bf351348dad
nguyenducduy2000 <nguyenducduy2000@gmail.com>
2023-11-19 01:32:35 +0700

    Set up base cho dự án

---
+++ AI-detection-backend/src/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 4000
+
+const route = require('./routes');
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const corsOption = {
+    origon: '*',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOption))
+
+app.use(methodOverride('_method'))
+
+app.use(morgan('combined'))
+
+// Routes init
+route(app)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
